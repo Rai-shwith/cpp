b03225fd13bf57c39a39057efeb596d0ac66af52
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-11-02 18:50:10 +0530

    average of 2 num

---
+++ LAB1/average_of_two.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>  //for getch()
+
+int main(){
+    int a,b;
+    float avg;
+    cout<<"Enter two numbers"<<endl;
+    cin>>a>>b;
+    avg=(a+b)/2.0;
+    cout<<"Average of "<<a<<" and "<<b<<" is "<<avg<<endl;
+    getch();
+    return 0;
+}
+
+/*
+OUTPUT:
+Enter two numbers
+5
+6
+Average of 5 and 6 is 5.5
+*/
