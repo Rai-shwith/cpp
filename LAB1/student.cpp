b671616c34ffae8e1534d7b5a0c44244ac86d9cc
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-11-02 19:08:26 +0530

    student class

---
+++ LAB1/student.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <conio.h> //for getch() comment this line if you using visual studio 2010 or older
+#include <string>
+
+using namespace std;
+
+class Student {
+        string name;
+        int roll;
+        float marks;
+    public:
+        void display() {
+            cout << "Name: " << name << endl;
+            cout << "Roll: " << roll << endl;
+            cout << "Marks: " << marks << endl;
+        }
+        void set(string n, int r, float m) {
+            name = n;
+            roll = r;
+            marks = m;
+        }
+};
+
+int main(){
+    Student s1;
+    string name;
+    int roll;
+    float marks;
+    cout << "Enter name, roll and marks of student" << endl;
+    cin >> name >> roll >> marks;
+    s1.set(name, roll, marks);
+    s1.display();
+    getch();
+    return 0;
+}
+
+/*
+OUTPUT:
+Enter name, roll and marks of student
+Ashwith
+29
+90
+Name: Ashwith 
+Roll: 29
+Marks: 90
+*/
