0889c88abe2295d9a079aa4a1b4ba4517aacf018
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-12-11 05:17:36 +0000

    chore: lab 7 p3 not completed

---
+++ LAB7/postfix_to_infix.cpp
@@ -0,0 +1,77 @@
+// Write a program to convert postfix expression to infix expression using stack.
+
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+class Stack
+{
+public:
+    char postfix[30];
+    int s[30];
+    int result;
+    int top;
+    Stack()
+    {
+        top = -1;
+    }
+
+    // Fuction to evaluate postfix expression
+    void evaluateExprssions()
+    {
+        for (int i = 0; i < strlen(postfix); i++)
+        {
+            char ch = postfix[i];
+            if (isdigit(ch))
+            {
+                s[++top] = ch - '0';
+            }
+            else
+            {
+                int op2 = s[top--];
+                int op1 = s[top--];
+                performOperation(op1, op2, ch);
+                s[++top] = result;
+            }
+        }
+    }
+
+    void performOperation(int op1,int op2,char symbol){
+        switch (symbol)
+        {
+        case '+':
+            result = op1 + op2;
+            break;
+        case '-':
+            result = op1 - op2;
+            break;
+        case '*':
+            result = op1 * op2;
+            break;
+        case '/':
+            result = op1 / op2;
+            break;
+        case '^':
+        case '$':
+            result = pow(op1, op2);
+            break;
+        }
+    }
+};
+
+int main()
+{
+    Stack ob;
+    cout << "Enter the postfix expression: ";
+    cin >> ob.postfix;
+    ob.evaluateExprssions();
+    cout << "Result: " << ob.result << endl;
+    return 0;
+}
+
+/*
+OUTPUT:
+
+*/
